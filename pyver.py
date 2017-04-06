67754bfe89eb5e747241baa32cffb9730ac7b965
Neal Gordon <nealagordon@gmail.com>
2017-04-06 08:00:43 -0700

    removed the file listing in the log file

--- pyver.py
+++ pyver.py
@@ -13,24 +13,24 @@ def pyver(user, comment, archivefiles):
 	main function of pyver. copys files from the current directory and creates an archive.
 	the tab-delimmeted log file pyver.csv keeps a record of the file changes
     '''
-    
+
     #user setting to create compressed zip files instead of copies of directories
-    ziparchive = False    
-    
+    ziparchive = False
+
     if not os.path.isdir('.archive'):
         print('not currently a pyver repo, creating archive file directory .archive')
         os.mkdir('.archive')
         pyverlog = open(os.path.join('.archive','pyver.log'),'a')
-        pyverlog.write('User   Timestamp   ArchiveSize   Comment   Files  \n')        
+        pyverlog.write('User      Timestamp      ArchiveSize      Comment  \n')
         try:
             make_hidden('.archive')
         except:
             print('failed to make .archive hidden, which only works on windows')
-        
+
     n = datetime.datetime.now()
-    timestamp = datetime.datetime.strftime(n, '%Y%m%d%H%M%S')  
+    timestamp = datetime.datetime.strftime(n, '%Y%m%d%H%M%S')
     archivepath = os.path.join('.archive',timestamp)
-    
+
 	### was used to copy an entire directory but was wasteful
     #shutil.copytree('.', archivepath, ignore=shutil.ignore_patterns('.archive'))
 
@@ -38,15 +38,15 @@ def pyver(user, comment, archivefiles):
     os.mkdir(archivepath)
     for f in archivefiles:
         if not os.path.exists(os.path.dirname(os.path.join(archivepath, f))):
-            os.makedirs(os.path.join(archivepath,os.path.dirname(f)))    
+            os.makedirs(os.path.join(archivepath,os.path.dirname(f)))
         try:
-            shutil.copy(f , os.path.join(archivepath, f))       
+            shutil.copy(f , os.path.join(archivepath, f))
             print(os.path.join(archivepath, f))
         except:
             print('%s copy failed. Is it open?' % os.path.join(archivepath, f))
         #print('directory %s created' % os.path.join(archivepath,os.path.dirname(f)))
     archivesize = sum([os.path.getsize(s) for s in os.listdir(archivepath)])/1e3 # kb
-    
+
     ### makes a zip file instead
     if ziparchive:
         shutil.make_archive(archivepath,'zip', archivepath)
@@ -55,16 +55,15 @@ def pyver(user, comment, archivefiles):
     archivefilesstr = '|'.join(archivefiles)
     ''' writes what files were archived'''
     pyverlog = open(os.path.join('.archive','pyver.log'),'a')
-    pyverlog.write('%s , %s , %ikb , %s , %s\n' % (user,
+    pyverlog.write('%s , %s , %i kb , %s \n' % (user,
                                            timestamp,
                                            archivesize,
-                                           comment,
-                                           archivefilesstr))
+                                           comment))
     pyverlog.close()
-	
+
 def make_hidden(hidedir):
     '''creates windows hidden folder
-    can also use the windows command 
+    can also use the windows command
     os.system("attrib +s +h "+.archive)
     '''
     import ctypes
@@ -76,8 +75,8 @@ def make_hidden(hidedir):
         raise ctypes.WinError()
 
 def log():
-    '''shows the contents of the pyver log file''' 
-    
+    '''shows the contents of the pyver log file'''
+
     if os.path.exists(os.path.join('.archive','pyver.log')):
         pyverlog = open(os.path.join('.archive','pyver.log'),'r')
         for k in pyverlog.readlines():
@@ -89,7 +88,7 @@ def log():
 def tree(path,indent=' '):
     '''recursiveley prints the contents of a directory'''
     for file in os.listdir(path):
-        fullpath = path + '/' + file              
+        fullpath = path + '/' + file
         if os.path.isfile(fullpath):
             print('%s     %s  ' % (indent,file ) )
         else:   # os.path.isdir
@@ -97,11 +96,11 @@ def tree(path,indent=' '):
             tree(fullpath,indent + '   |')
 
 def make_win_exe():
-    '''creates a windows exe file that allows the pyver program to be 
+    '''creates a windows exe file that allows the pyver program to be
         run without the windows interpreter
     '''
     os.system('pip install pyinstaller')
-    os.system('pyinstaller pyver.py --onefile')    
+    os.system('pyinstaller pyver.py --onefile')
 
 def all_files(rootDir = '.', wildcard='.'):
     '''
@@ -114,22 +113,22 @@ def all_files(rootDir = '.', wildcard='.'):
         for fname in fileList:
             fullpath = os.path.join(dirName, fname)
             # skip over files and folders that start with . or ~
-            
+
             # os.path.splitext(f[-2])[1]      extension
             #if fullpath.split(os.sep)[1] != '.' and fullpath.split(os.sep)[1] != '~':
             #os.path.basename(fullpath)[0] != '.'
-            #os.path.basename(fullpath)[0] != '~'            
+            #os.path.basename(fullpath)[0] != '~'
             if '.' not in [x[0] for x in fullpath.split(os.sep)[1:]] and \
                '~' not in [x[0] for x in fullpath.split(os.sep)[1:]]:
-                #print(fullpath)  
+                #print(fullpath)
                 fileext = os.path.splitext(fullpath)[1]
                 if wildcard == fileext:
                     files.append(fullpath)
-                    print('%s added %s' % (wildcard,fullpath)) 
+                    print('%s added %s' % (wildcard,fullpath))
                 elif wildcard == '.':
                     files.append(fullpath)
                 else:
-                    print('extension %s not found or skipping' % (fileext)) 
+                    print('extension %s not found or skipping' % (fileext))
     files = [x[2:] for x in files] # clear off .\\
     return files
 
@@ -162,32 +161,32 @@ if __name__=='__main__':
     '''
 	executed if module is not imported
     '''
-    
+
     if len(sys.argv) == 2 and sys.argv[1] == 'log':
         log()
     elif len(sys.argv) == 2 and sys.argv[1] == 'tree':
         tree('.')
-    else:    
-       
+    else:
+
         p = argparse.ArgumentParser()
-        p.add_argument('-u', '--user', 
-                       default = os.path.split(os.path.expanduser('~'))[-1], 
+        p.add_argument('-u', '--user',
+                       default = os.path.split(os.path.expanduser('~'))[-1],
                        help='''user that made the pyver entry''')
         p.add_argument('-f', '--files',
-                       help='''omit to add all files. 
+                       help='''omit to add all files.
 								add extensions EXAMPLE "*.txt|*.docx"
 								add files separated by vertical line EXAMPLE "file1.txt|file2.txt" ''')
-        p.add_argument('-c', '--comment', default = '', 
-                       help='''add a comment enclosed in double quotes 
-                               as to what changed. EXAMPLE "This is my comment" ''')                             
+        p.add_argument('-c', '--comment', default = '',
+                       help='''add a comment enclosed in double quotes
+                               as to what changed. EXAMPLE "This is my comment" ''')
         args = p.parse_args()
-        
+
         if args.files:
             # collect all files of a type
             tempfiles = args.files.split('|')
             args.files = []
             for t in tempfiles:
-                if t[0] == '*':	
+                if t[0] == '*':
                     args.files.extend(all_files('.', t[1:]))
                 elif t not in os.listdir(os.getcwd()):
                     print('%s not found, skipping' % t)
@@ -195,13 +194,12 @@ if __name__=='__main__':
                     args.files.append(t)
                 #if '.archive' in args.files:
                 #    args.files.remove('.archive')
-                
+
         else:
             #files = os.listdir(os.getcwd())  ## does not capture subdirectories
             args.files = all_files('.','.')  # add all files
             #if '.archive' in args.files:
             #    args.files.remove('.archive')
-        #user, archivefiles, comment = args.user, args.files, args.comment        
+        #user, archivefiles, comment = args.user, args.files, args.comment
         #print(args.files)
-        pyver(args.user, args.comment, args.files)      
-        
+        pyver(args.user, args.comment, args.files)
